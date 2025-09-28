ed8646ea70b7f3ed5f7d19dfbf8f2f91a2511216
Piotr Wittchen <piotr@wittchen.io>
2025-09-29 00:13:59 +0200

    added comment with data sources to be used for reading live data / current conditions

--- src/main/java/com/github/pwittchen/varun/service/CurrentConditionsService.java
+++ src/main/java/com/github/pwittchen/varun/service/CurrentConditionsService.java
@@ -4,4 +4,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CurrentConditionsService {
+
+    /*
+     *    Data sources to use:
+     *
+     *       AT:
+     *
+     *       https://www.kiteriders.at/wind/weatherstat_kn.html
+     *
+     *       PL:
+     *
+     *       www.wiatrkadyny.pl/wiatrkadyny.txt - dla stacji Kadyny
+     *       www.wiatrkadyny.pl/krynica/wiatrkadyny.txt - dla stacji Krynica Morska
+     *       www.wiatrkadyny.pl/kuznica/wiatrkadyny.txt - dla stacji Kuźnica
+     *       www.wiatrkadyny.pl/draga/wiatrkadyny.txt - dla stacji Draga
+     *       www.wiatrkadyny.pl/rewa/wiatrkadyny.txt - dla stacji Rewa
+     *       www.wiatrkadyny.pl/puck/wiatrkadyny.txt - dla stacji Puck
+     */
 }
