36143721cdde889e2908b2cf04bedd74610c9c20
Piotr Wittchen <piotr@wittchen.io>
2025-09-28 21:10:49 +0200

    updated minify.sh bash script to update api endpoint url to the real (not-mocked) url

--- prototype/frontend/minify.sh
+++ prototype/frontend/minify.sh
@@ -7,3 +7,4 @@ npx html-minifier-terser index.full.html -o index.min.html \
   --minify-css true
 sed -r 's/ {2,}/  /g' index.min.html > index.html
 rm index.min.html
+sed -i '' "s|http://localhost:8001/mock-data.json|/api/v1/spots|g" index.html
