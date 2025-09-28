8d8f8f7bfa84ab5b9a53d94eb22d4b21fa099d0e
Piotr Wittchen <piotr@wittchen.io>
2025-09-28 21:05:13 +0200

    added logs for the REST API

--- src/main/java/com/github/pwittchen/varun/controller/SpotsController.java
+++ src/main/java/com/github/pwittchen/varun/controller/SpotsController.java
@@ -2,6 +2,8 @@ package com.github.pwittchen.varun.controller;
 
 import com.github.pwittchen.varun.model.Spot;
 import com.github.pwittchen.varun.service.AggregatorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,8 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/v1/")
 public class SpotsController {
 
+    private static final Logger log = LoggerFactory.getLogger(SpotsController.class);
+
     private final AggregatorService aggregatorService;
 
     public SpotsController(AggregatorService aggregatorService) {
@@ -20,11 +24,13 @@ public class SpotsController {
 
     @GetMapping("spots")
     public Flux<Spot> spots() {
+        log.info("GET /api/v1/spots");
         return Flux.fromIterable(aggregatorService.getSpots());
     }
 
     @GetMapping("health")
     public Mono<String> health() {
+        log.info("GET /api/v1/health");
         return Mono.just("UP");
     }
 }
