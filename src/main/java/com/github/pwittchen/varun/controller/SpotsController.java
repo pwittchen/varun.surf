9a3c106ad2a5e3437fb680600f31d025ed896b4d
Piotr Wittchen <piotr@wittchen.io>
2025-10-05 13:35:35 +0200

    [wip] integrated ollama with Spring Boot using LLM model: smollm2:135m (now, it's a temp solution just to verify that it works - currently just on the localhost)

--- src/main/java/com/github/pwittchen/varun/controller/SpotsController.java
+++ src/main/java/com/github/pwittchen/varun/controller/SpotsController.java
@@ -2,6 +2,7 @@ package com.github.pwittchen.varun.controller;
 
 import com.github.pwittchen.varun.model.Spot;
 import com.github.pwittchen.varun.service.AggregatorService;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +16,30 @@ import java.util.Map;
 public class SpotsController {
     private final AggregatorService aggregatorService;
 
-    public SpotsController(AggregatorService aggregatorService) {
+    ChatClient chatClient;
+
+    public SpotsController(AggregatorService aggregatorService, ChatClient chatClient) {
         this.aggregatorService = aggregatorService;
+        this.chatClient = chatClient;
     }
 
     @GetMapping("spots")
     public Flux<Spot> spots() {
         return Flux.fromIterable(aggregatorService.getSpots());
+    }
+
+    //PLEASE NOTE: you need to run ollama serve command in order to use this
+    //TODO: this is just a placeholder example - it needs to be moved to aggregator and then used there in the right way
+
+    @GetMapping("ai")
+    public Mono<String> ai() {
+        return chatClient
+                .prompt()
+                .user("tell me a joke")
+                .stream()
+                .content()
+                .collectList()
+                .map(list -> String.join("", list));
     }
 
     @GetMapping("health")
