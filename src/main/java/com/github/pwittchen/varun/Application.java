8c059308e031df806d32b222daa95a70e63be711
Piotr Wittchen <piotr@wittchen.io>
2025-10-06 12:04:33 +0200

    removed enablecaching annotation

--- src/main/java/com/github/pwittchen/varun/Application.java
+++ src/main/java/com/github/pwittchen/varun/Application.java
@@ -2,12 +2,10 @@ package com.github.pwittchen.varun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableRetry
-@EnableCaching
 @EnableScheduling
 @SpringBootApplication
 public class Application {
