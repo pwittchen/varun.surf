e4b23f911c1db05ea6b31033cf82f78d59391b3e
Piotr Wittchen <piotr@wittchen.io>
2025-10-23 18:39:46 +0200

    fixed AsyncConfig (previous version was incomplete)

--- src/main/java/com/github/pwittchen/varun/config/AsyncConfig.java
+++ src/main/java/com/github/pwittchen/varun/config/AsyncConfig.java
@@ -2,16 +2,24 @@ package com.github.pwittchen.varun.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
 @EnableAsync
-public class AsyncConfig {
-    @Bean(name = "virtualThreadExecutor")
-    public Executor virtualThreadExecutor() {
-        return Executors.newThreadPerTaskExecutor(Thread.ofVirtual().factory());
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Bean(name = "virtualThreadExecutor", destroyMethod = "shutdown")
+    public ExecutorService virtualThreadExecutor() {
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return virtualThreadExecutor();
     }
 }
